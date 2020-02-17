dafd439a47574e7106a2e04b8dab262b24ed05ab
Mondaoporosis <kimwy1997@gmail.com>
2020-02-17 12:58:37 +0900

    separated all the controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
 export const home = (req, res) => res.send("Home");
 export const search = (req, res) => (req, res) => res.send("search");
+export const videos = (req, res) => (req, res) => res.send("Videos");
+export const upload = (req, res) => (req, res) => res.send("Upload");
+export const videoDetail = (req, res) => (req, res) => res.send("Video Detail");
+export const editVideo = (req, res) => (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => (req, res) => res.send("Delete Video");
