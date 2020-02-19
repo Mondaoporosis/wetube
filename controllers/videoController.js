cbbf6bcce6f12863e605134effb2b69146fabbfe
Mondaoporosis <kimwy1997@gmail.com>
2020-02-20 02:50:29 +0900

    Add search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,12 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req; // It's the same as "const searchingBy = req.query.term;"
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
