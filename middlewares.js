103ff85b75085a85ac4b3f2441120ad28be4869f
Mondaoporosis <kimwy1997@gmail.com>
2020-02-23 23:22:47 +0900

    Delete video folder

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,9 @@
 import multer from "multer";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "videos/" });
+const multerVideo = multer({ dest: "upload/videos/" });
+// Remember not to add '/' in front of the dest.
+// Adding '/' will make the function think that the destination is inside of my project file. (It will create that directory)
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
