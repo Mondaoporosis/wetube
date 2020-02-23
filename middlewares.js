c1993b6c61e0fa0288b9042ea96cc6dd07c811fb
Mondaoporosis <kimwy1997@gmail.com>
2020-02-23 23:37:27 +0900

    Fix video not playing

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "upload/videos/" });
+const multerVideo = multer({ dest: "uploads/videos/" });
 // Remember not to add '/' in front of the dest.
 // Adding '/' will make the function think that the destination is inside of my project file. (It will create that directory)
 
