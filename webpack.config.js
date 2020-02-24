35f1d4d4e798353dcabeb3761b07f9ade436ee4b
Mondaoporosis <kimwy1997@gmail.com>
2020-02-24 15:51:11 +0900

    Set webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // Webpack.config.js can't use modern JS (like import/export), so we have to use older JS.
 
 const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
@@ -9,10 +11,37 @@ const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
   entry: ENTRY_FILE,
   mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/, // 확장자 식별, scss 찾기. 만약 이걸 찾으면 아래 코드를 사용(use)
+        use: ExtractCSS.extract([
+          // This 'use' runs from bottom to top.
+          // So sass-loader -> postcss-loader -> css-loader -> extract
+          // Each of the loaders can be installed with npm install
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browers: "cover 99.5%" })]; // Use plugin called 'autoprefixer' with an option 'browsers: "cover 99.5%"'
+              }
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ])
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
-  }
+    filename: "[name].js"
+  },
+  plugins: [new ExtractCSS("styles.css")]
 };
 
 module.exports = config;
