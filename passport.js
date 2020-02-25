5b6b4bf665fc9b333a5930f00244851ab5a9cf1c
Mondaoporosis <kimwy1997@gmail.com>
2020-02-25 16:24:44 +0900

    Make passport handle user authentication

--- passport.js
+++ passport.js
@@ -2,3 +2,8 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Make passport handle user authentication
