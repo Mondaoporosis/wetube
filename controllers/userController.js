4d7cd8db2c1ac01d02bc4ffe15024f1870c00566
Mondaoporosis <kimwy1997@gmail.com>
2020-02-25 21:47:49 +0900

    Make userDetail page

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,8 +41,9 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
-    _json: { id, avatar_url, name, email }
+    _json: { id, avatar_url: avatarUrl, name, email }
   } = profile;
+  console.log(_, __, profile, cb);
   try {
     const user = await User.findOne({ email });
     if (user) {
@@ -54,7 +55,7 @@ export const githubLoginCallback = async (_, __, profile, cb) => {
       email,
       name,
       githubId: id,
-      avatarUrl: avatar_url
+      avatarUrl
     });
     return cb(null, newUser);
   } catch (error) {
@@ -69,6 +70,10 @@ export const postGithubLogin = (req, res) => {
 export const logout = (req, res) => {
   req.logout();
   res.redirect(routes.home);
+};
+
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
 
 export const userDetail = (req, res) =>
