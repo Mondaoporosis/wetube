998ccfc62ef51da93b8cbb78c0708da850a70deb
Mondaoporosis <kimwy1997@gmail.com>
2020-02-29 09:17:57 +0900

    Add creator to Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -38,10 +38,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
-  console.log(newVideo);
-  // To Do: Upload and save video
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -50,7 +51,8 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator"); //Types.ObjectId 유형에만 적용 가능.
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
